b37182b28f606ea2d288f5fa94ce00600bcf106e
codingbyTaekong <kumi7214@gmail.com>
2022-07-31 23:16:18 +0900

    init

---
+++ pages/admin.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Header from '../components/Header'
+
+const Admin: NextPage = () => {
+  return (
+    <div>
+        <Header />
+    </div>
+  )
+}
+
+export default Admin
