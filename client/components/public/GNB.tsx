d5922899f9325432f4a57c9a88cd1031a0daffcd
codingbyTaekong <kumi7214@gmail.com>
2022-10-18 22:30:33 +0900

    클라이맥스 인덱스페이지 개발 중

--- client/components/public/GNB.tsx
+++ client/components/public/GNB.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import styles from '../../styles/public/gnb.module.css'
 function GNB() {
@@ -6,8 +7,15 @@ function GNB() {
       <nav className={styles.nav}>
         <ul>
             <li>
-                <h1 className={styles.title}>로고</h1>
+                <h1 className={styles.title}>
+                  <Link href="/">
+                    클라이맥스
+                  </Link>
+                </h1>
             </li>
+              <li className={styles.loginButton}>
+                <Link href="/signin">로그인</Link>
+              </li>
         </ul>
       </nav>
     </header>
